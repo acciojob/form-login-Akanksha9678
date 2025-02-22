637e4e4185c3629c867ec5fe4389b2d5818fcdbd
Akanksha Kushwaha <78742130+Akanksha9678@users.noreply.github.com>
2025-02-22 21:32:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ function getFormvalue() {
     //Write your code here
 	let firstName=document.querySelector('input[name="fname"]').value;
 	let lastName=document.querySelector('input[name="lname"]').value;
+	let output=document.querySelector('input[value="Submit"]')
 	form1.addEventListener("click",
-	alert(firstName+" "+lastName)
+	window.alert(firstName+" "+lastName)
 	)
 }
