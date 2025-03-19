459c9d72bc2afe15708477d29dc104ed47a4f30f
Akanksha Kushwaha <78742130+Akanksha9678@users.noreply.github.com>
2025-03-19 23:22:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-function getFormvalue(e) {
-    //Write your code here
-            e.preventDefault();
-	let firstName=document.querySelector('input[name="fname"]').value;
-	let lastName=document.querySelector('input[name="lname"]').value;
-	alert(firstName + " " + lastName);
-}
+        function getFormvalue(event) {
+            event.preventDefault();
+            let firstName = document.querySelector("input[name='fname']").value.trim();
+            let lastName = document.querySelector("input[name='lname']").value.trim();
+            if (firstName && lastName) {
+                alert(`${firstName} ${lastName}`);
+            } else {
+                alert("Please enter both first and last names.");
+            }
+        }
+        
+        document.getElementById("nameForm").addEventListener("submit", getFormvalue);
